2b003bcb2f4d8010458f3004c3376af5d2d08d6e
edward.shao <edward.shao@innocellence.com>
2016-06-21 09:28:52 +0800

    changed001

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ http.createServer(function handler(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
 }).listen(8888, '127.0.0.1');
+
 console.log('Server running at http://127.0.0.1:8888/');
